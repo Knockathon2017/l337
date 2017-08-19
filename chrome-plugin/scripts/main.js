05f9dfb409caaed40c8f5611bc3ef6bff0c2913a
Atmaram S <atmarams@exzeo.com>
2017-08-19 08:24:41 +0530

    Fixed a bug with Vue templates

--- chrome-plugin/scripts/main.js
+++ chrome-plugin/scripts/main.js
@@ -1,7 +1,16 @@
 const app = new Vue({
   el: '#face-trace',
   data: {
-    moods: {},
+    moods: {
+      anger: 0,
+      contempt: 0,
+      disgust: 0,
+      fear: 0,
+      happiness: 0,
+      neutral: 0,
+      sadness: 0,
+      surprise: 0
+    },
     overallMood: ""
   },
 });
